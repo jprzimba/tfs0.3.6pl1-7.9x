0539766eac0493205aa8675e647efb1124e02c98
João Paulo <jprzimba@gmail.com>
2024-10-15 09:24:24 -0300

    * Burst arrows using old formula based on magic level.

--- data/weapons/scripts/burst_arrow.lua
+++ data/weapons/scripts/burst_arrow.lua
@@ -1,10 +1,8 @@
 local combat = createCombatObject()
-setCombatParam(combat, COMBAT_PARAM_BLOCKARMOR, 1)
-setCombatParam(combat, COMBAT_PARAM_BLOCKSHIELD, 1)
-setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
+setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_FIREDAMAGE)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_FIREAREA)
 setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_BURSTARROW)
-setCombatFormula(combat, COMBAT_FORMULA_SKILL, 1, 0, 1, 0)
+setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, 0, 0, 0, -150)
 
 local area = createCombatArea({
 	{1, 1, 1},
